7ebda4d5a98bb8668ad90f5e5f93d28e25cf57cc
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-15 10:41:46 +0530

    writing into file

--- index.js
+++ index.js
@@ -63,6 +63,34 @@ const fsPromise = require('fs').promises;
 // fileReading();
 
 
+// writing into file
+
+const textFile = path.join(__dirname, "files", "text.txt");
+const content = "I am loving node js series";
+
+// fs.writeFile(textFile, content, (err) => {
+//     if (err) throw new Error('something went wrong');
+//     console.log('write operation completed successfully');
+//     fs.readFile(textFile, 'utf-8', (err, data) => {
+//         if (err) throw new Error(err)
+//         console.log(data)
+//     })
+// });
+
+const writingInFile = async () => {
+    try {
+
+        await fsPromise.writeFile(textFile,"\n it's awesome", {flag:'a+'});
+        // await fsPromise.appendFile(textFile, '\n this is the file appender');
+        const data = await fsPromise.readFile(textFile,'utf-8')
+        console.log(data)
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+writingInFile();
 
 
 
