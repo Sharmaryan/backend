a65dca31bc0659c4a8f5de2270c26178cfa360f3
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-22 18:41:07 +0530

    download file

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-res.sendFile(path.resolve(__dirname + '/index.html'));
+res.sendFile(path.resolve(__dirname) + '/index.html');
 });
+
 app.get('/about',(req,res) => {
-res.sendFile(path.resolve(__dirname + '/about.html'));
+res.sendFile(path.resolve(__dirname) + '/about.html');
 });
 
+app.get('/download',(req,res) => {
+res.download(path.resolve(__dirname) + '/about.html');
+});
 
+app.use(express.static('public'))
 
 app.listen(PORT, () =>
     console.log(`server is running on port ${PORT}`) 
