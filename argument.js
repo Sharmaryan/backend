f8fe3fe4e5cd760bea9d01cfaf5172317af1ad11
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-22 18:16:47 +0530

    default route express

--- argument.js
+++
@@ -1,9 +0,0 @@
-// console.log(process.argv.slice(2)[0]);
-
-// process.argv.forEach((value, index) => {
-//     console.log(`${index}:${value}`);
-// })
-const minimist = require('minimist');
-
-const argNew = minimist(process.argv.slice(2));
-console.log(argNew.name);
