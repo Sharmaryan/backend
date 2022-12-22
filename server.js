7f0bb187a19d459e61660c838938441bd73873a5
Aryan Sharma <panditaryan.as@gmail.com>
2022-12-22 18:28:10 +0530

    sending files

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-res.send('hello from express')
-})
+res.sendFile(path.resolve(__dirname + '/index.html'));
+});
+app.get('/about',(req,res) => {
+res.sendFile(path.resolve(__dirname + '/about.html'));
+});
+
+
 
 app.listen(PORT, () =>
-    console.log(`server is running on port ${PORT}`)
+    console.log(`server is running on port ${PORT}`) 
 );
